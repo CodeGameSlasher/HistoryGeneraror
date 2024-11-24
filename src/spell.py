b6d93f0650c8807f20b3c31e21a87c8abd157ce8
Code Slasher <ivavoronov@mail.ru>
2024-11-24 19:05:48 +0300

    Update spell.py

--- src/spell.py
+++ src/spell.py
@@ -1,4 +1,5 @@
 from string import punctuation
+from random import choice
 
 from spellchecker import SpellChecker
 
@@ -9,13 +10,13 @@ def correct(text: str) -> str:
     # find those words that maybe misspelled
     misspelled = spell.unknown(text.translate(str.maketrans(' ', ' ', punctuation)).split(' '))
 
-    correct_list: dict = {''.join({word}): spell.correction(word) for word in misspelled if
-                          word != spell.correction(word)}
+    correct_list: dict[str, str] = {''.join({word}): spell.correction(word) for word in misspelled if word != spell.correction(word) and isinstance(spell.correction(word), str)}
     print(correct_list)
     corrected_text = ''
     for word in text.lower().split():
+        word = word.translate(str.maketrans(' ', ' ', punctuation))
         print(word)
-        if word in correct_list.keys():
+        if correct_list.get(word):
             corrected_text += correct_list.get(word) + ' '
         else:
             corrected_text += word + ' '
@@ -23,5 +24,5 @@ def correct(text: str) -> str:
 
 
 if __name__ == "__main__":
-    result = correct('Кагда тебя спать?')
+    result = correct('Ало,')
     print(result)
