79535227ca6e258adef2b84bc25b9da23a5061f0
Code Slasher <ivavoronov@mail.ru>
2024-11-27 18:17:14 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
-from time import time
-from json import loads, dumps
-from random import choice, seed
+from __init__ import *
 
 from rich.console import Console
 from speakerpy.lib_speak import Speaker
@@ -20,62 +18,6 @@ class Voicer(Speaker):
     def say(self, text, samplerate=48000):
         for chunk in SeleroText(text, self.language).chunk():
             self.speak(chunk, samplerate)
-
-
-class WordList(list[str]):
-    def __init__(self, words: list[str]):
-        words = [word.strip('\n\r\t ') for word in words if word]
-        self.extend(words)
-
-    @classmethod
-    def from_file(cls, filename: str = 'words.txt', encoding: str = 'utf-8'):
-        for encode in [encoding, 'utf-8', 'windows-1251']:
-            try:
-                with open(filename, 'r', encoding=encode) as file:
-                    words = [loads(dumps(dict(word=word), ensure_ascii=False)).get('word') for word in file.readlines()]
-                    break
-            except Exception as e:
-                console.print(f'[red]{e}')
-        
-        return cls(words)
-    
-    def save_to_file(self, filename: str = 'words.txt', encoding = 'utf-8'):
-        with open(filename, 'w', encoding=encoding) as file:
-            file.write('\n'.join(self))
-
-class History(list[str]):
-    def __init__(self, history: list[str] = []):
-        self.extend(history)
-
-    def __str__(self):
-        return '' if len(self) < 0 else ' '.join(self).strip().replace('  ', ' ').capitalize()
-
-class HistoryGenerator:
-    def __init__(self, words: WordList):
-        self.words = words
-        self.history = History()
-        self.seed = int(time() + len(words))
-
-    @property
-    def story(self):
-        return str(self.history)
-    
-    @property
-    def word_list(self):
-        return self.words.copy()
-    
-    def set_seed(self, value: str):
-        if not isinstance(value, int):
-            self.seed = int.from_bytes(bytes(value, encoding='utf-8') if isinstance(value, str) else bytes(value))
-        self.history = History()
-        seed(self.seed)
-    
-    def generate(self, word_count: int = 1):
-        if word_count < 0:
-            raise ValueError(f'{word_count} < 0!')
-        else:
-            self.history.extend(choice(self.words) for _ in range(word_count))
-        return self.story
         
 
 if __name__ == '__main__':
